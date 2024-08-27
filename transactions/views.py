383e8b4309a43c863bb7418c1ba0b1702d19cc92
Edward Sung <edwardsung4217@gmail.com>
2024-08-26 23:15:53 -0400

    Added Chart
    Need to add color to database when adding a vendor

--- transactions/views.py
+++ transactions/views.py
@@ -1,8 +1,19 @@
-
-# transactions/views.py
 from django.shortcuts import render
 from .models import Transaction
+from collections import defaultdict
+import json
 
 def dashboard(request):
-    transactions = Transaction.objects.all()  # Fetch all transactions
-    return render(request, 'dashboard.html', {'transactions': transactions})
+    transactions = Transaction.objects.all().order_by('date')  # Ascending order (oldest first)
+    # Prepare data for the chart
+    chart_data = defaultdict(lambda: {'dates': [], 'amounts': []})
+
+    for transaction in transactions:
+        chart_data[transaction.vendor]['dates'].append(str(transaction.date))
+        chart_data[transaction.vendor]['amounts'].append(float(transaction.amount))  # Convert Decimal to float
+
+    # Convert defaultdict to a regular dict and then to JSON
+    chart_data = json.dumps(chart_data)
+    print(chart_data)
+
+    return render(request, 'dashboard.html', {'chart_data': chart_data, 'transactions': transactions})
