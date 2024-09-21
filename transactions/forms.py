a7ba37bc6f2f78e2d3e352a5a9497f4c2ef63dc6
edwardsung4217 <edwardsung4217@gmail.com>
2024-09-21 18:23:04 -0400

    Small text change

--- transactions/forms.py
+++ transactions/forms.py
@@ -7,7 +7,7 @@ class EmailFilterForm(forms.ModelForm):
         model = EmailFilter
         fields = ['vendor', 'email_domain', 'bill_identifier', 'category']
         labels = {
-            'vendor': 'vendor',
+            'vendor': 'Vendor',
             'email_domain': 'Email Domain',
             'bill_identifier': 'Preceding Text of Bill Amount',
             'category': 'Category',
